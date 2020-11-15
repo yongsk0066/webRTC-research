a54c391359c82b36aa043e4d9c4a20ab1ea86729
yongsk0066 <yongsk0066@naver.com>
2020-11-15 15:10:48 +0900

    port test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ ReactDOM.render(
     signaling={{
       dispatcher: dispatcher,
       url:
-        process.env.NODE_ENV !== "production" ? "http://localhost:3001" : "https://yongseok.shop:443",
+        process.env.NODE_ENV !== "production" ? "http://52.79.51.112:8080" : "https://yongseok.shop:443",
     }}
   >
     <UserMediaProvider constraints={{ audio: true, video: true }}>
